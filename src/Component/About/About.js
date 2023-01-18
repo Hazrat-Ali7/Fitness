bc4090a50b8667bf473c17547c803f9fba5677bf
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-01-19 00:52:58 +0600

    code added

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import about from "../images/about.png";
+ import about from "../images/about.png";
 const About = () => {
   return (
     <div className="about-page container">
@@ -12,10 +12,11 @@ const About = () => {
         <h1>About Us</h1>
 
         <p>
-        Hello I'm Hazrat Ali.I am currently pursuing BSC in CSE. I want to be a Software Engineer in near future.
-Now I am currently a MERN Stack Stack and I have passion in web technologies. I am a fast learner and I am confident about myself.
-I believe I can face chanllenges of this always changing tech industry.I am looking forward to face the upcoming challenges.Talking about my hobby I love to read
-books related to programming and I love to develop things  with programming languages.Thanks for visiting my profile
+Hello I'm Hazrat Ali.I am currently pursuing BSC in CSE. I want to be a Software Engineer in near future.
+Now I am currently a MERN Stack Developer and I have passion in Web technologies. I am a fast learner and I am confident about myself.
+I believe I can face chanllenges of this always changing tech industry.I am looking forward to face the upcoming challenges.
+Talking about my hobby I love to read books related to programming and I love to develop things  with programming languages.Thanks for visiting my profile.
+
         </p>
         <button className="btn btn-primary">Learn More</button>
       </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,19 @@
-
-// Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDSG8sSKKQPM42_XyH_mFQXH2PlEEcQu2w",
-  authDomain: "assignment-10-84623.firebaseapp.com",
-  projectId: "assignment-10-84623",
-  storageBucket: "assignment-10-84623.appspot.com",
-  messagingSenderId: "1050382353836",
-  appId: "1:1050382353836:web:e8153a776b3eef00e96609"
+  apiKey: "AIzaSyCUuOISs0Z6saRWLTIlu77pv6nL4EnuqVg",
+  authDomain: "hazrat-fitness.firebaseapp.com",
+  projectId: "hazrat-fitness",
+  storageBucket: "hazrat-fitness.appspot.com",
+  messagingSenderId: "100518942528",
+  appId: "1:100518942528:web:3766d0bfafa5c8903e4ca5",
+  measurementId: "G-3Q7XFCQQ90"
 };
 
 // Initialize Firebase
