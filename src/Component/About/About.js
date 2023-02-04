d4d833d8b13bbb9ef11965746e5b16d4ab0a2fbc
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-02-04 19:06:17 +0600

    blogs code added

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -9,15 +9,18 @@ const About = () => {
         <img src={about} alt="" />
       </div>
       <div className="about-right-side">
-        <h5>Fithness Gym</h5>
+        <h5>Fitness Gym</h5>
         <h1>About Us</h1>
 
         <p>
-Hello I'm Hazrat Ali.I am currently pursuing BSC in CSE. I want to be a Software Engineer in near future.
-Now I am currently a MERN Stack Developer and I have passion in Web technologies. I am a fast learner and I am confident about myself.
-I believe I can face chanllenges of this always changing tech industry.I am looking forward to face the upcoming challenges.
-Talking about my hobby I love to read books related to programming and I love to develop things  with programming languages.Thanks for visiting my profile.
-
+          Hello I'm Hazrat Ali.I am currently pursuing BSC in CSE. I want to be
+          a Software Engineer in near future. Now I am currently a MERN Stack
+          Developer and I have passion in Web technologies. I am a fast learner
+          and I am confident about myself. I believe I can face chanllenges of
+          this always changing tech industry.I am looking forward to face the
+          upcoming challenges. Talking about my hobby I love to read books
+          related to programming and I love to develop things with programming
+          languages.Thanks for visiting my profile.
         </p>
         <button className="btn btn-primary">Learn More</button>
       </div>
